61904503d851ee3342068df49bfaeb879e9f34fd
priyanshu maurya <priyanshumaurya@priyanshus-MacBook-Air-3.local>
2026-04-01 14:21:17 +0530

    If_else_.js

---
+++ If_else.js
@@ -0,0 +1,36 @@
+let temperature=25;
+if(temperature>30){
+    console.log("it's Hot!");
+}else if(temperature>20){
+    console.log("It's warm.");
+}else{
+console.log("It's cold.")
+}
+let day=8;
+switch(day){
+    case 1:
+    console.log("Monday");
+    break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("wednesday")
+        break;
+    case 4:
+        console.log("thursday");
+        break;
+    case 5:
+        console.log("friday");
+        break;
+    case 6:
+    console.log("saturday");
+    break;
+    case 7:
+        console.log("sunday");
+        break;
+    default:
+        console.log("invailid day");
+        break;
+
+}
