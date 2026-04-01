8bbd19a61e155dd3c6c0a1bd4890f1b45834ab64
priyanshu maurya <priyanshumaurya@priyanshus-MacBook-Air-3.local>
2026-04-01 14:25:40 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("hellow world");
+// var a=10;
+// var a=20;
+// var b=30;
+// var c=a+b;
+// console.log(c);
+// console.log("let introduction");
+// let a=10;
+// let a=20;
+// console.log(a);
+//•	Comparison Operators: == (loose equality), === (strict equality), !=, !==, >, <, >=, <=.
+let x=10;
+let y="10";
+console.log(x==y);
+console.log(x===y);
+console.log(x!=y);
+console.log(x!==y);
